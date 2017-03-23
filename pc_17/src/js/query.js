e3a91d42eb8ae1d6b527ebb59ee1e2c3b67834f9
superkevingit <superkevingit@gmail.com>
2017-03-23 13:11:21 +0800

    完成瀑布流

--- pc_17/src/js/query.js
+++ pc_17/src/js/query.js
@@ -3,24 +3,29 @@
  */
 import Mustache from './mustache';
 
-var render = function (url, template) {
-    $.get(url, function (data) {
-        var contentWrap = $(".content__wrap");
-        if ( data.count ) {
-            var count = data.count,
-                list = data.list,
-                i = 0;
-            for (i; i < count; i++) {
-                var unix = list[i]['time'];
-                list[i]['time'] = handleTime(unix);
-                list[i]['type'] = list.type === 'found' ? '招领' : '丢失';
-                var content = Mustache.to_html(template, list[i]);
-                contentWrap.append(content);
+var render = function (url, template, contentWrap) {
+    $.ajax({
+        url: url,
+        type: 'GET',
+        async: false,
+        success: function (data) {
+            if (data.count) {
+                var count = data.count,
+                    list = data.list,
+                    i = 0;
+                for (i; i < count; i++) {
+                    var unix = list[i]['time'];
+                    list[i]['time'] = handleTime(unix);
+                    list[i]['type'] = list.type === 'found' ? '招领' : '丢失';
+                    var content = Mustache.to_html(template, list[i]);
+                    contentWrap.append(content);
+                }
+            } else {
+                contentWrap.text("好像没有信息～～");
             }
-        } else {
-            contentWrap.text("好像没有信息～～");
         }
     });
+
 
     function handleTime(unix) {
         var time = new Date( unix*1000 );
--- pc_17/src/main.js
+++ pc_17/src/main.js
@@ -5,12 +5,23 @@ import 'normalize.css';
 import '../src/style/style.less';
 import ball from './js/particles';
 import render from './js/query';
+import {waterfall} from './js/masonry';
 
-$(document).ready(
-    ball(),
+$(document).ready( function () {
     (function () {
-        var baseUrl = 'api.php/';
-        var template = require('../src/template.html');
-        render(baseUrl, template);
+        var contentWrap = $(".content__wrap");
+        var template = $("#template").html();
+
+        ball();
+
+        (function () {
+            var baseUrl = 'api.php/';
+            render(baseUrl, template, contentWrap);
+        })();
+
+        (function () {
+            waterfall(contentWrap);
+        })();
+
     })()
-);
+});
